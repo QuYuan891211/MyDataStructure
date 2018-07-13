56fe1e1d2a363c20517845137e1364d413ae541d
quyuan <quyuan891211@163.com>
2018-07-13 19:15:55 +0800

    Standard linked list: Bubble sort

--- MyDataStructure_Java/src/LinkedList/standard/TestNode.java
+++ MyDataStructure_Java/src/LinkedList/standard/TestNode.java
@@ -13,7 +13,7 @@ public class TestNode {
     }
     public int getLength(){
        Node temp = head;
-       int length = 1;
+       int length = 0;
        while (temp.getNext() != null){
            length ++;
            temp = temp.getNext();
@@ -38,7 +38,7 @@ public class TestNode {
         temp.setNext(null);
     }
     public void insertNodeByIndex(int index,Node newNode){
-        if(index < 1 && index > getLength()-1){
+        if(index < 1 && index > getLength()){
             System.out.println("index error");
             return;
         }
@@ -54,7 +54,7 @@ public class TestNode {
         temp.setNext(newNode);
     }
     public void deleteNodeByIndex(int index){
-        if(index < 1 && index > getLength()-1){
+        if(index < 1 && index > getLength()){
             System.out.println("index error");
             return;
         }
@@ -64,18 +64,60 @@ public class TestNode {
         }
         temp.setNext(temp.getNext().getNext());
     }
+    public Node getNodeByIndex(int index){
+        if(index < 1 && index > getLength()-1){
+            System.out.println("index error");
+            return head;
+        }
+        Node temp = head;
+        for(int i = 1;i<index;i++){
+            temp = temp.getNext();
+        }
+        return temp.getNext();
+    }
+    //冒泡排序（bubble sort）
+    public void order(){
+        Node temp = head;
+        if(getLength()<2){
+            return;
+        }
+        boolean flag;
+        for(int i = 1;i<getLength();i++){
+            flag = false;
+            for(int j = getLength()-1; j>=i;j--){
+                if(getNodeByIndex(j).getData() > getNodeByIndex(j+1).getData()){
+                    flag = true;
+                    Node temp1 = getNodeByIndex(j);
+                    Node temp2 = getNodeByIndex(j+1);
+                    int tempData = temp1.getData();
+                    temp1.setData(temp2.getData());
+                    temp2.setData(tempData);
+                }
+            }
+            if (!flag){ break;}
+        }
+    }
+
     public static void main(String[] args){
-        Node newNode1 = new Node(4);
-        Node newNode2 = new Node(2);
-        Node newNode3 = new Node(5);
+        Node newNode1 = new Node(7);
+        Node newNode2 = new Node(0);
+        Node newNode3 = new Node(1);
+        Node newNode4 = new Node(8);
+        Node newNode5 = new Node(9);
+        Node newNode6 = new Node(5);
         TestNode testNode = new TestNode();
         testNode.addNode(newNode1);
         testNode.addNode(newNode2);
         testNode.addNode(newNode3);
+        testNode.addNode(newNode4);
+        testNode.addNode(newNode5);
+        testNode.addNode(newNode6);
         testNode.deleteNode();
-        Node newNode4 = new Node(66);
-        testNode.insertNodeByIndex(2,newNode4);
+        Node insertNode = new Node(2);
+        testNode.insertNodeByIndex(3,insertNode);
         testNode.deleteNodeByIndex(1);
+        System.out.println(" get node by index: "+ testNode.getNodeByIndex(5).getData());
+        testNode.order();
         testNode.print();
         System.out.println("Length: " + testNode.getLength());
     }
